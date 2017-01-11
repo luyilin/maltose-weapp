c33df6687ede428bdc48990df004914347073cb7
luyilin <luyilin12@gmail.com>
2017-01-11 17:31:06 +0800

    init

---
+++ pages/maltose/maltose.js
@@ -0,0 +1,90 @@
+Page({
+    data: {
+      textareaFocus: '#999',
+      focus: false,
+      emoticon: [],
+      tab: [],
+      showMaltose: true,
+      showEmoticon: false,
+      showEmoji: true
+    },
+    codeTap: function(e) {
+      // wx.navigateTo({
+      //   url: '../logs/logs',
+      //   success: function(res){
+      //     console.log('success')
+      //   },
+      //   fail: function() {
+      //     console.log('fail')
+      //   }
+      // })
+    },
+    maltoseFocus: function(e) {
+      this.setData({
+        textareaFocus: 'lightsalmon',
+        textareaValue: e.detail.value
+      })
+    },
+    maltoseBlur: function(e) {
+      this.setData({
+        textareaFocus: '#999'
+      })
+    },
+    bindFormSubmit: function() {
+      let show = !this.data.showMaltose
+      this.setData({
+        focus: true,
+        showMaltose: show
+      })
+    },
+    chooseTab: function(e) {
+      var tab = e.target.id
+      for(let i = 0; i < this.data.emoticon.length; i++) {
+        var j = Object.keys(this.data.emoticon[i])[0]
+        if (tab === j) {
+          if (j.indexOf('Emoji') === -1) {
+            this.toggle(false, true)
+          } else {
+            this.toggle(true, false)
+          }
+        }
+      }
+    },
+    toggle: function(i, j) {
+      this.setData({
+        showEmoticon: i,
+        showEmoji: j
+      })
+    },
+    appendHtml: function(e) {
+      if(e.target.id) {
+        let newValue = this.data.textareaValue + e.target.id
+        this.setData({
+          textareaValue: newValue
+        })
+      }
+    },
+    onLoad: function() {
+      console.log('maltose onload')
+      var that = this
+      wx.request({
+        url: `https://luyilin.github.io/Maltose/demo/maltose2.json`,
+        data: {},
+        dataType: 'json',
+        method: 'GET',
+        success: function(res){
+          console.log('request success', res.data)
+          that.setData({
+            emoticon: res.data,
+            tab: Object.keys(res.data)
+          })
+        },
+        fail: function() {
+          console.log('request fail')
+        },
+        complete: function() {
+          // complete
+        }
+      })
+    }
+})
